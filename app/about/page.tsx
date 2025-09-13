3458be7a6a1ca2e4fc9036d5b01608ee735ce132
Isaiah Quindo <isaiah.quindo@gmail.com>
2025-09-13 13:27:23 +1000

    Update About page content to reflect a collective mission and improve layout consistency
    - Changed the narrative from a personal perspective to a collective one, using "we" instead of "I" to foster community engagement.
    - Adjusted CSS for the main container to improve layout consistency across different screen sizes.

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -10,24 +10,24 @@ const about = async () => {
   return (
     <div className="flex flex-col min-h-screen bg-gray-50">
       <Header locations={uniqueLocations} coffeeShops={coffeeShops} />
-      <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 flex-1 bg-white border border-gray-200 rounded-lg my-6">
+      <main className="max-w-7xl mx-6 md:mx-auto px-4 sm:px-6 lg:px-8 py-8 flex-1 bg-white border border-gray-200 rounded-lg my-6">
         <div className="w-full flex flex-col gap-4">
           <h1 className="text-3xl font-bold text-gray-900">About Us</h1>
           <p className="text-gray-600">
-            At RateMyCoffee, my mission is simple: to share genuine experiences
+            At RateMyCoffee, our mission is simple: to share genuine experiences
             and honest reviews about coffee shops from fellow coffee lovers.
-            This site started as a personal project born from my passion for
-            coffee and my love of discovering great local spots. I’m Isaiah
-            Quindo, based in Sydney, and I created this platform to bring
-            together a community where coffee enthusiasts can help each other
-            find the best brews, atmospheres, and places to relax.
+            This site started as a personal project born from our passion for
+            coffee and love of discovering great local spots. We created this
+            platform to bring together a community where coffee enthusiasts can
+            help each other find the best brews, atmospheres, and places to
+            relax.
           </p>
           <p className="text-gray-600">
-            I want to be upfront—RateMyCoffee is not perfect yet. It’s a work in
-            progress, and I’m committed to making it better over time, adding
-            features and improving based on your feedback. Every review, rating,
-            and story shared helps build a more vibrant and useful resource for
-            all coffee lovers who visit.
+            We want to be upfront, RateMyCoffee is not perfect yet. It's a work
+            in progress, and we're committed to making it better over time,
+            adding features and improving based on your feedback. Every review,
+            rating, and story shared helps build a more vibrant and useful
+            resource for all coffee lovers who visit.
           </p>
           <p className="text-gray-600">
             Thank you for being part of this journey. Together, we can celebrate
